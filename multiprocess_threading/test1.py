d02f167c00870779899d61ead5818d2035ada563
sun xiong <sungnox@outlook.com>
2017-09-10 19:37:54 +0800

    multi thread

--- multiprocess_threading/test1.py
+++ multiprocess_threading/test1.py
@@ -1,66 +1,39 @@
 # -*- coding:utf-8 -*-
-#多线程并不能提高计算密集型任务的速度
-#但可以提高IO密集型任务的速度
-from threading import Thread
 import threading
 import time
-threading.currentThread()
-#计算密集型任务
-#采用多线程运行的时间不比单线程小
-def CN():
-    print(threading.currentThread())
-    i = 1
-    for k in range(20000000):
-        # time.sleep(0.001)
-        i += 1
-    print('i:',i)
-    print('N')
 
+#用Condion实现生产消费模型
+#Condion默认与Rlock关联，一个线程拥有一个锁
+cn = threading.Condition()
+productor = None
 
-def CP():
-    print(threading.currentThread())
-    i = 1
-    for k in range(50000000):
-        # time.sleep(0.001)
-        i += 1
-    print('i:',i)
-    print('P')
+def product():
+    global productor
+        if cn.acquire():
+	        while True:
+		            if productor is None:
+			                    print('Beging product something...')
+					                    productor ='things'
+							                    cn.notify()
+									                cn.wait()
+											            time.sleep(2)
 
+												    def consume():
+												        global productor
+													    if cn.acquire():
+													            while True:
+														                if productor is not None:
+																                print('Beging consume...')
+																		                productor = None
+																				                cn.notify()
+																						            cn.wait()
+																							                time.sleep(2)
 
-#IO密集型任务
-#就算是IO密集型任务，加锁之后也不能提高速度
-# lock = threading.Lock()
-def SN():
-    i = 1
-    # lock.acquire()
-    for k in range(2):
-        time.sleep(1)
-        i += 1
-    # lock.release()
-    print('i:',i)
-    print('N')
+																									prdct = threading.Thread(target=product)
+																									cmsm = threading.Thread(target=consume)
 
-def SP():
-    i = 1
-    # lock.acquire()
-    for k in range(5):
-        time.sleep(1)
-        i += 1
-    # lock.release()
-    print('i:',i)
-    print('P')
+																									cmsm.start()
+																									prdct.start()
+																									# prdct.join()
+																									# cmsm.join()
 
-
-if __name__ == '__main__':
-
-    # t1 = Thread(target=CN)
-    # t2 = Thread(target=CP)
-    star = time.time()
-    # t1.start()
-    # t2.start()
-    # t1.join()
-    # t2.join()
-    CN()
-    CP()
-    en = time.time()
-    print(-star+en)
